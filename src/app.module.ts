27a7be71677c515ae4e856c83e535d063d098d94
hiroki0116 <hirokiseino0116@gmail.com>
2023-06-24 20:30:30 +1000

    :sparkles: Build a scafold

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BalanceModule } from './balance/balance.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule, UserModule, BalanceModule, PrismaModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
